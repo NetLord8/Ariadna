e10a32e45ea59305150e7e949d211ebcbcf023f3
Toad User <Toad.User@ToadWorld.com>
2016-05-13 15:01:21 +0300

    Updated from Toad again

--- ARIADNA_SQL/Double Invitro.sql
+++ ARIADNA_SQL/Double Invitro.sql
@@ -1,8 +1,15 @@
 /*–езультаты повтор€ющихс€ нализов от »нвитро, повтор€ющиес€ по счетам*/
-select to_date(to_char(PE.DAT,'dd/mm/yyyy'),'dd/mm/yyyy'),pa.num,PA.LASTNAME,se.text,be.code,count(*)
+(select to_date(to_char(PE.DAT,'dd/mm/yyyy'),'dd/mm/yyyy'),pa.num,PA.LASTNAME,se.text,count(*)
 from bill_ext be, invoice_ext ie, patserv pe ,srvdep se,patient pa
 where ie.billid = be.keyid and be.company_ext_id=39001 and PE.KEYID=IE.PATSERVID and PE.SRVDEPID=SE.KEYID
 and PA.KEYID=PE.PATIENTID
-group by to_char(PE.DAT,'dd/mm/yyyy'),pa.num,PA.LASTNAME,se.text,be.code
-having count(*)>1
-order by to_date(to_char(PE.DAT,'dd/mm/yyyy'),'dd/mm/yyyy')
+group by to_char(PE.DAT,'dd/mm/yyyy'),pa.num,PA.LASTNAME,se.text
+having count(*)>1)
+minus
+(select to_date(to_char(PE.DAT,'dd/mm/yyyy'),'dd/mm/yyyy'),pa.num,PA.LASTNAME,se.text,count(*)
+from bill_ext be, invoice_ext ie, patserv pe ,srvdep se,patient pa
+where ie.billid = be.keyid and be.company_ext_id=39001 and PE.KEYID=IE.PATSERVID and PE.SRVDEPID=SE.KEYID
+and PA.KEYID=PE.PATIENTID
+group by to_char(PE.DAT,'dd/mm/yyyy'),pa.num,PA.LASTNAME,se.text,be.code,be.dat
+having count(*)>1)
+/*order by to_date(to_char(PE.DAT,'dd/mm/yyyy'),'dd/mm/yyyy')*/
